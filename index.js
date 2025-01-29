fd038962f030eff6b779cd19e1939b042ea8fd04
ajuarezse <ajuarez.se@gmail.com>
2025-01-29 08:56:19 -0800

    add gitignore

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.get("/users/:id", (req, res) => {
+  if (!users[req.params.id]) {
+    res.send(`This user doesn't exist`);
+    return;
+  }
+  const { name, age } = users[req.params.id];
+
+  res.send(`User ${name}, ${age} years old`);
+});
+
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
 });
