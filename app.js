71e12a26c7ef73c53604ad1da676ee6281c4374a
ajuarezse <ajuarez.se@gmail.com>
2025-01-31 23:04:48 -0800

    install and configure mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const { PORT = 3001 } = process.env;
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/jarocho_db")
+  .then(() => {})
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
