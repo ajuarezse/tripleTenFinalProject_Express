b4c7a1a78dd57b44bf47711f687249e485f5e9c8
ajuarezse <ajuarez.se@gmail.com>
2025-02-05 23:47:00 -0800

    configure starting route

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/users");
 
-const { PORT = 3001 } = process.env;
 const app = express();
+const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/jarocho_db")
-  .then(() => {})
+  .then(() => {
+    console.log("conntected to DB");
+  })
   .catch(console.error);
 
+app.use("/", userRouter);
+
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 30,
+  },
+  avatar: {},
+});
 
 module.exports = mongoose.model("user", userSchema);
