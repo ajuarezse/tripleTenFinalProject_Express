a464aa77bc9f633a8eb6aad13c107da4702b92b8
ajuarezse <ajuarez.se@gmail.com>
2025-02-06 15:16:04 -0800

    add error catch

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,7 @@ const getUsers = (req, res) => {
     .then((users) => res.send(users))
     .catch((err) => {
       console.error(err);
+      return res.status(500).send({ message: err.message });
     });
 };
 
