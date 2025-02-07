15b47096515d20869cc5d75b516c6dedd36b19e6
ajuarezse <ajuarez.se@gmail.com>
2025-02-07 11:12:14 -0800

    add constitent error handling

--- controllers/users.js
+++ controllers/users.js
@@ -5,31 +5,38 @@ const getUsers = (req, res) => {
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      return res.status(500).send({ message: "Internal server error" });
     });
 };
 
 const createUser = (req, res) => {
   const { name } = req.body;
+  if (!name) {
+    return res.status(400).send({ message: "Name is required" });
+  }
   User.create({ name })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
-      console.log(err.name);
-      return res.status(500).send({ message: err.message });
+      return res.status(500).send({ message: "Internal server error" });
     });
 };
 
 const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
-    .then((user) => res.status(200).send(user))
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      return res.status(200).send(user);
+    })
     .catch((err) => {
       console.error(err);
-      if (err.name === "") {
-        //
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid user ID format" });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(500).send({ message: "Internal server error" });
     });
 };
 
