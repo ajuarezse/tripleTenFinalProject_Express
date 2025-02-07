fe0b0806b94ab443b0c644b6668a282e1d7b0f26
ajuarezse <ajuarez.se@gmail.com>
2025-02-07 10:21:59 -0800

    initiate get and create user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -2,11 +2,21 @@ const User = require("../models/user");
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send(users))
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
       return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports = { getUsers };
+const createUser = (req, res) => {
+  const { name } = req.body;
+  User.create({ name })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser };
