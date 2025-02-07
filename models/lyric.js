e295ea74c3cea57e4d8e03d164b44bac4b42347a
ajuarezse <ajuarez.se@gmail.com>
2025-02-07 14:55:22 -0800

    configure schema details

--- models/lyric.js
+++ models/lyric.js
@@ -1,5 +1,31 @@
 const mongoose = require("mongoose");
 
-const lyricSchema = new mongoose.Schema({});
+const lyricSectionSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  notes: { type: String },
+  likes: { type: Number, default: 0 },
+  likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  contributor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  dateAdded: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model("lyrics", lyricSchema);
+const lyricSchema = new mongoose.Schema({
+  songName: { type: String, required: true },
+  author: { type: String },
+  tags: [{ type: String }],
+  dateFirstEntered: { type: Date, default: Date.now },
+  verses: [lyricSectionSchema],
+  choruses: [lyricSectionSchema],
+  otherSections: [
+    {
+      sectionType: { type: String, required: true },
+      lyrics: [lyricSectionSchema],
+    },
+  ],
+});
+
+module.exports = mongoose.model("Lyric", lyricSchema);
