fd291435ce55b0e4729cbfdc90e5147599a93a75
ajuarezse <ajuarez.se@gmail.com>
2025-02-08 09:04:22 -0800

    add user schema details

--- models/user.js
+++ models/user.js
@@ -1,13 +1,76 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  // Basic Profile Information
   name: {
     type: String,
     required: true,
     minLength: 2,
     maxLength: 30,
   },
-  avatar: {},
+  avatar: {
+    type: String,
+    default: "https://example.com/default-avatar.png",
+  },
+  bio: {
+    type: String,
+    maxLength: 500,
+  },
+
+  // Authentication
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v), // Basic email validation
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  // Social Features
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  likedLyrics: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Lyric",
+    },
+  ],
+  contributedLyrics: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Lyric",
+    },
+  ],
+
+  // Additional Fields
+  lastLogin: {
+    type: Date,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
