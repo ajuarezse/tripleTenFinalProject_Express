36ffa4faa2f11cd17954038e936d3dec388ba089
ajuarezse <ajuarez.se@gmail.com>
2025-02-11 10:47:16 -0800

    configure update user

--- controllers/users.js
+++ controllers/users.js
@@ -40,4 +40,34 @@ const getUser = (req, res) => {
     });
 };
 
+const updateUser = (req, res) => {
+  const { userId } = req.params;
+  const { name, avatar, location, bio } = req.body;
+
+  if (!name && !avatar && !location && !bio) {
+    return res.status(400).send({ message: "No fields provided to update" });
+  }
+
+  User.findByIdAndUpdate(
+    userId,
+    { name, avatar, location, bio },
+    { new: true, runValidators: true }
+  )
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid user ID format" });
+      }
+      return res.status(500).send({ message: "Internal server error" });
+    });
+};
+
+const deleteUser = (req, res) => {};
+
 module.exports = { getUsers, createUser, getUser };
