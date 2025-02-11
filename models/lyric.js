6e0929c80dae8d7821ec4679f0ba276751a8ed04
ajuarezse <ajuarez.se@gmail.com>
2025-02-10 22:43:36 -0800

    add creater field indexing versioning and timestamps

--- models/lyric.js
+++ models/lyric.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-//item section
 const lyricSectionSchema = new mongoose.Schema({
   text: { type: String, required: true },
   notes: { type: String },
@@ -14,20 +13,30 @@ const lyricSectionSchema = new mongoose.Schema({
   dateAdded: { type: Date, default: Date.now },
 });
 
-//item
-const lyricSchema = new mongoose.Schema({
-  songName: { type: String, required: true },
-  author: { type: String },
-  tags: [{ type: String }],
-  dateFirstEntered: { type: Date, default: Date.now },
-  verses: [lyricSectionSchema],
-  choruses: [lyricSectionSchema],
-  otherSections: [
-    {
-      sectionType: { type: String, required: true },
-      lyrics: [lyricSectionSchema],
+const lyricSchema = new mongoose.Schema(
+  {
+    songName: { type: String, required: true },
+    author: { type: String },
+    tags: [{ type: String, minlength: 2, maxlength: 20 }],
+    dateFirstEntered: { type: Date, default: Date.now },
+    verses: [lyricSectionSchema],
+    choruses: [lyricSectionSchema],
+    otherSections: [
+      {
+        sectionType: { type: String, required: true },
+        lyrics: [lyricSectionSchema],
+      },
+    ],
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
-});
+    version: { type: Number, default: 1 },
+  },
+  { timestamps: true }
+);
+
+lyricSchema.index({ songName: "text" });
 
 module.exports = mongoose.model("Lyric", lyricSchema);
