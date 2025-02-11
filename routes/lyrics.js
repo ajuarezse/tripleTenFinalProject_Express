9f884c3f4e89c01bc1f1633cf5f281e8c576656e
ajuarezse <ajuarez.se@gmail.com>
2025-02-10 22:58:07 -0800

    route lyric actions

--- routes/lyrics.js
+++ routes/lyrics.js
@@ -1,1 +1,16 @@
 const router = require("express").Router();
+const {
+  getLyrics,
+  createLyrics,
+  getLyricById,
+  updateLyric,
+  deleteLyric,
+} = require("../controllers/lyrics");
+
+router.get("/", getLyrics);
+router.post("/", createLyrics);
+router.get("/:lyricId", getLyricById);
+router.put("/:lyricId", updateLyric);
+router.delete("/:lyricId", deleteLyric);
+
+module.exports = router;
